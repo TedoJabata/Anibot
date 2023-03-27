c0541a8651ac7a2ffb8e7b61ad0fc8502ebc856b
Anifrog <58802773+TedoJabata@users.noreply.github.com>
2023-03-27 20:38:44 +0300

    added some validatioins

--- Commands/Moderation/reloadslashes.js
+++ Commands/Moderation/reloadslashes.js
@@ -3,7 +3,11 @@ const { RegisterSlashCommands } = require("../../SlashRegistrant")
 module.exports = {
     name: 'reloadslashes',
     execute: async(message) => {
-        RegisterSlashCommands(['Fun', 'Math', 'Moderation'])
-        await message.channel.send(`Reloaded all slash commands!`);
+        if (message.author.id == '978754737031761960') {
+            RegisterSlashCommands(['Fun', 'Math', 'Moderation'])
+            await message.channel.send(`Reloaded all slash commands!`);
+        } else {
+            await message.reply(`You don't have the premission to reload commands!`);
+        }
     }
 };
--- Commands/Slash/Moderation/reloadslashes.js
+++ Commands/Slash/Moderation/reloadslashes.js
@@ -6,7 +6,11 @@ module.exports = {
         .setName('reloadslashes')
         .setDescription('Reloads all slash commands.'),
     async execute(interaction) {
-        RegisterSlashCommands(['Fun', 'Math', 'Moderation'])
-        await interaction.reply(`Reloaded all slash commands!`);
+        if (interaction.user.id == '978754737031761960') {
+            RegisterSlashCommands(['Fun', 'Math', 'Moderation'])
+            await interaction.reply(`Reloaded all slash commands!`);
+        } else {
+            await interaction.reply(`You don't have the premission to reload commands!`);
+        }
     },
 };
