44e288e02679fe436665249d14bd9b92bf327a84
Anifrog <58802773+TedoJabata@users.noreply.github.com>
2023-04-06 01:19:48 +0300

    fixed add and added subtract prefix function

--- Commands/Math/subtract.js
+++ Commands/Math/subtract.js
@@ -1,25 +1,30 @@
 module.exports = {
-    name: 'add',
+    name: 'subtract',
     execute: async(message, args) => {
         if (args.length < 1) {
             await message.channel.send(`<@${message.member.id}> give me some numbers!`)
             return
         }
 
-        let sum = 0;
+        let result = null
         let validNumbers = false
 
         args.forEach(element => {
+
             if (Number(element)) {
-                validNumbers = true
-                sum += Number(element)
+                if (result == null) {
+                    result = Number(element)
+                    validNumbers = true
+                } else {
+                    result -= Number(element)
+                }
             }
         })
 
         if (!validNumbers) {
             await message.channel.send(`<@${message.member.id}> give me valid numbers!`)
         } else {
-            await message.channel.send(`<@${message.member.id}> The sum is ***${String(sum)}***`)
+            await message.channel.send(`<@${message.member.id}> The result is ***${String(result)}***`)
         }
     }
 }
